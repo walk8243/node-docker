d8866c5dcaf243963f774c07a3ffd9486de95da9
walk8243 <walk8243@gmail.com>
2020-05-04 18:03:05 +0900

    Intlの日本語の確認も行う

--- icu.js
+++ icu.js
@@ -1,8 +1,10 @@
 const january = new Date(9e8);
 const english = new Intl.DateTimeFormat('en', { month: 'long' });
 const spanish = new Intl.DateTimeFormat('es', { month: 'long' });
+const japanese = new Intl.DateTimeFormat('ja', { month: 'long' });
 const defaultLanguage = new Intl.DateTimeFormat(undefined, { month: 'long' });
 
 console.log(english.format(january));
 console.log(spanish.format(january));
+console.log(japanese.format(january));
 console.log(defaultLanguage.format(january));
